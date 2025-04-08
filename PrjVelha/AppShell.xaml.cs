a1be2ff27667e47f056281e3a98dd138b7023993
LabInfo <LabInfo@TERREOA2M13>
2025-04-07 22:14:52 -0300

    Adicionar arquivos de projeto.

---
+++ PrjVelha/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PrjVelha
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PrjVelha/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PrjVelha
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
